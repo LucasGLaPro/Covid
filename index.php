987e3812f38d7aa3c0279e91d7e38988f58f0f70
LucasGLaPro <lghyselen@la-providence.net>
2021-03-23 15:40:06 +0100

    Afffiche les user

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php 
 echo"coucou";
+
+$MaBase = new PDO('mysql:host=mysql-lucaslapro.alwaysdata.net; dbname=lucaslapro_virus; charset=utf8','229070_lucas', 'lucaslapro');
+$ObjetResultatDeRequeteBrut = $MaBase->query("SELECT * FROM `user` ORDER BY nom ASC");
+while($TableauDunTuple = $ObjetResultatDeRequeteBrut->fetch())
+echo $TableauDunTuple['nom'];
 ?>
